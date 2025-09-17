4e783cbaad3602e4ea4ea4ce27daf899b779df9a
dwuggh <dwuggh@gmail.com>
2025-09-17 19:13:58 +0800

    refactor: update expr parser with new token types and parsing logic

--- src/core/parser/expr_parser.rs
+++ src/core/parser/expr_parser.rs
@@ -1,3 +1,6 @@
+use std::cell::{Cell, RefCell};
+use std::rc::Rc;
+
 use chumsky::extra::SimpleState;
 use chumsky::input::{Stream, ValueInput};
 use chumsky::inspector::Inspector;
@@ -6,8 +9,10 @@ use rustc_hash::{FxHashMap, FxHashSet};
 
 use crate::core::env::Environment;
 use crate::core::ident::Ident;
-use crate::core::parser::expr::{Expr, Literal, Number};
-use crate::core::parser::node::Node;
+use crate::core::parser::expr::{
+    new_refbox, Args, Expr, If, Let, Literal, Number, Progn, SpecialForm, Var,
+};
+use crate::core::parser::lexer::Token;
 
 // type NodeInput<'s> = impl Input<'s, Node, SimpleSpan>;
 
@@ -16,12 +21,16 @@ struct FunctionFrame {
     option_args: Vec<Ident>,
     rest_args: Option<Ident>,
     captures: FxHashSet<Ident>,
+    id: u32,
 }
 
-struct LexicalScope {
-    binds: Vec<Ident>,
-    recursive: bool,
-}
+type LexicalScope = Rc<Vec<(Ident, Option<Expr>)>>;
+
+// struct LexicalScope {
+//     binds: Vec<Ident>,
+//     recursive: bool,
+//     id: u32,
+// }
 
 enum Scope {
     Function(FunctionFrame),
@@ -33,7 +42,7 @@ struct ScopeChain<'a> {
     scopes: Vec<Scope>,
     current: usize,
     prev: Option<usize>,
-    env: &'a Environment
+    env: &'a Environment,
 }
 
 impl<'a> ScopeChain<'a> {
@@ -47,11 +56,13 @@ impl<'a> ScopeChain<'a> {
             env,
         }
     }
+
+    fn resolve(&self, ident: Ident) {}
 }
 
 impl<'a, 'src, I> Inspector<'src, I> for ScopeChain<'a>
 where
-    I: ValueInput<'src, Token = Node, Span = SimpleSpan>,
+    I: ValueInput<'src, Token = Token<'src>, Span = SimpleSpan>,
 {
     type Checkpoint = usize;
 
@@ -75,13 +86,14 @@ where
     }
 }
 
-type Error<'s> = extra::Err<Rich<'s, Node>>;
-// type Extra<'s> = extra::Full<Rich<'s, Node>, SimpleState<>>;
+type Error<'s> = extra::Err<Rich<'s, Token<'s>>>;
+type Extra<'s> = extra::Full<Rich<'s, Token<'s>>, (), ()>;
 
-fn parser<'s, I>() -> impl Parser<'s, I, Expr, extra::Err<Rich<'s, Node>>>
+fn parser<'s, I>() -> impl Parser<'s, I, Expr, Extra<'s>>
 where
-    I: ValueInput<'s, Token = Node, Span = SimpleSpan>,
+    I: ValueInput<'s, Token = Token<'s>, Span = SimpleSpan>,
 {
+    let id = 0;
     recursive(|expr| {
         // let binding = select! {
         //     Node::Sexp(nodes) if nodes.len() == 2 => {
@@ -96,26 +108,117 @@ where
         //     }
         // };
 
-        let ident = any::<I, Error>().try_map(|node, span| match node {
-            Node::Ident(ident) => Ok(Expr::Symbol(ident)),
+        let refexpr = expr.clone().map(RefCell::new).boxed();
+        let vecref = refexpr.clone().repeated().collect::<Vec<_>>().boxed();
+        let boxref = expr.clone().map(new_refbox);
+
+        let ident = any::<I, Extra>().try_map(|node, span| match node {
+            Token::Ident(ident) => Ok(ident),
             _ => Err(Rich::custom(span, "Expected identifier")),
         });
+        let ident_interned = ident.map(|ident| Ident::from(ident));
 
-        let sexp = any::<I, Error>().try_map(|node, span| match node {
-            Node::Sexp(sexp) => Ok(sexp),
-            _ => Err(Rich::custom(span, "Expected identifier")),
-        });
+        let keyword = |kw| just(Token::Ident(kw)).ignored();
 
-        //  let bindings
-        let bind = choice((ident.clone().then(expr.clone()), ident.clone()));
-        let binds = bind.clone().repeated();
+        let nil = keyword("nil")
+            .or(just(Token::LParen).ignore_then(just(Token::RParen).ignored()))
+            .map(|_| Expr::Nil);
+
+        let vector = vecref
+            .clone()
+            .delimited_by(just(Token::LBracket), just(Token::RBracket));
+
+        let progn = vecref.clone().map(Progn::new).boxed();
+
+        let symbol = ident.map(|ident| Cell::new(Var::Symbol(ident.into())));
+
+        let bindings = choice((
+            ident_interned.then(refexpr.map(Option::Some)),
+            ident_interned.map(|ident| (ident, None)),
+        ))
+        .repeated()
+        .collect::<Vec<_>>()
+        .map(Rc::new)
+        .boxed();
+
+        let let_expr = sexp(keyword("let").ignore_then(bindings).then(progn.clone())).map(
+            |(bindings, body)| Expr::SpecialForm(SpecialForm::Let(Let { bindings, body, id: 1 })),
+        ).boxed();
+
+        let if_expr = sexp(
+            keyword("if")
+                .ignore_then(boxref.clone())
+                .then(boxref.clone())
+                .then(progn.clone()),
+        )
+            .map(|((cond, then), els)| Expr::SpecialForm(SpecialForm::If(If { cond, then, els }))).boxed();
+
+        let normal_arg = ident
+            .try_map(|ident, span| {
+                if ident.starts_with('&') {
+                    Err(Rich::custom(span, "unexpected &"))
+                } else {
+                    Ok(Ident::from(ident))
+                }
+            })
+            .boxed();
+        let optional_arg = keyword("&optional")
+            .ignore_then(normal_arg.clone())
+            .repeated()
+            .at_least(1)
+            .collect::<Vec<_>>()
+            .boxed();
+
+        let args = normal_arg
+            .clone()
+            .repeated()
+            .collect::<Vec<_>>()
+            .then(optional_arg.or_not())
+            .then(keyword("&rest").ignore_then(normal_arg.clone()).or_not())
+            .map(|((normal, optional), rest)| {
+                let args = Args {
+                    normal,
+                    optional,
+                    rest,
+                };
+                Rc::new(args)
+            })
+            .boxed();
+
+        let string = any::<I, Extra>()
+            .try_map(|node, span| match node {
+                Token::Str(str) => Ok(str),
+                _ => Err(Rich::custom(span, "Expected string")),
+            })
+            .boxed();
+        let interactive = keyword;
+        // let lambda = keyword("lambda")
+        //     .ignore_then()
 
         let primitive = select! {
-            Node::Integer(n) => Expr::new_int(n),
-            Node::Float(f) => Expr::new_float(f),
-            Node::Char(c) => Expr::new_char(c),
-            Node::Str(s) => Expr::new_str(s),
+            Token::Integer(n) => Expr::new_int(n),
+            Token::Float(f) => Expr::new_float(f),
+            Token::Character(c) => Expr::new_char(c),
+            Token::Str(s) => Expr::new_str(s),
         };
-        choice((primitive,))
+        choice((primitive, let_expr, if_expr))
     })
+}
+
+fn sexp<'s, I, O, E>(exprs: impl Parser<'s, I, O, E>) -> impl Parser<'s, I, O, E>
+where
+    I: ValueInput<'s, Token = Token<'s>, Span = SimpleSpan>,
+    E: extra::ParserExtra<'s, I>,
+{
+    exprs.delimited_by(just(Token::LParen), just(Token::RParen))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_expr() {
+        // parser().
+    }
 }
